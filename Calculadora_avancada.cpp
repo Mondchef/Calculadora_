ea0d3cd981db213830eec258d5b69aa6d53ec37e
Gabriel Fernandes <gabrielfernandes.b.c@gmail.com>
2019-04-26 00:15:34 -0300

    Adicionando raiz quadrada para a calculadora

--- Calculadora_avancada.cpp
+++ Calculadora_avancada.cpp
@@ -4,6 +4,17 @@
 #include<stdlib.h>
 #include <string.h>
 
+float soma(float s1, float s2){
+	float resultado;
+	resultado = s1 + s2;
+	return resultado;
+}
+float raiz(float r1, float r2){
+	float resultado;
+	resultado = pow(r1, 1/r2);
+	return resultado;
+}
+
 main(){
 	setlocale(LC_ALL,"portuguese");	
 	int saida1;
@@ -27,6 +38,19 @@ main(){
 		scanf("%f %c %f", &n1, &operador, &n2);
 		switch(operador){
 			
+				case '+':
+					r = soma(n1,n2);
+					printf("\n%.2f", r);
+					break;	
+				case 'v':
+    				r = raiz(n1,n2);
+    				printf("\n%.2f", r);
+    				break;
+    			case 'V':
+    				r = raiz(n1,n2);
+    				printf("\n%.2f", r);
+    				break;
+			
 		}
 	
 			printf("\nSe deseja continuar na calculadora digite 0\nSe não qualquer número:\n");
@@ -41,5 +65,5 @@ main(){
     		}
     	}
 	}
-}
+
 

f0bf9448de5d79130e89b7f5715b667187b4a4ee
Mondchef <gabrielfernandes.b.c@gmail.com>
2019-04-26 12:39:05 -0300

    MASTER COMPLETO

--- Calculadora_avancada.cpp
+++ Calculadora_avancada.cpp
@@ -4,9 +4,61 @@
 #include<stdlib.h>
 #include <string.h>
 
+float soma(float s1, float s2){
+	float resultado;
+	resultado = s1 + s2;
+	return resultado;
+}
+
+float divisao(float d1, float d2){
+	float resultado;
+	resultado = d1 / d2;
+	return resultado;
+}
+
+float subtracao(float sub1, float sub2){
+	float resultado;
+	resultado = sub1 - sub2;
+	return resultado;
+}
+
+float multiplicacao(float m1, float m2){
+	float resultado;
+	resultado = m1 * m2;
+	return resultado;
+}
+
 float porcentagem(float p1, float p2){
 	float resultado;
 	resultado = (p1 * p2)/100;
+	return resultado;
+}
+
+float raizquadrada(float r1, float r2){
+	float resultado;
+	resultado = (r1);
+	return resultado;
+}
+
+float exponencial(float q1, float q2){
+	float resultado;
+	resultado = pow(q1,q2);
+	return resultado;
+}
+
+
+float calcula_log(double base, double rlog){
+	int a;
+	double cont;
+	for(cont=0, a=1; a<=rlog; cont++, a++){
+		a *= base;
+	}
+	return cont;
+	}
+
+float raizquadrada(float r1){
+	float resultado;
+	resultado = sqrt(r1);
 	return resultado;
 }
 	
@@ -33,11 +85,65 @@ main(){
 		scanf("%f %c %f", &n1, &operador, &n2);
 		switch(operador){
 			
-				case '%':
-    				r = porcentagem(n1,n2);
+			case '+':
+				r = soma(n1,n2);
+				printf("\n%.2f", r);
+				break;	
+			case '-':
+    			r = subtracao(n1,n2);
+    			printf("\n%.2f", r);
+    			break;
+    		
+    		case '*':
+    			r = multiplicacao(n1,n2);
+    			printf("\n%.2f", r);
+    			break;
+    			
+			case '/':
+  	 			if(n2==0){
+  	 	 			printf("O resultado é indefinido");
+				}
+					else{
+    				r = divisao(n1,n2);
     				printf("\n%.2f", r);
     				break;
-    				
+    			}
+    		
+    		case '%':
+    			r = porcentagem(n1,n2);
+    			printf("\n%.2f", r);
+    			break;
+    		
+    		case '^':
+    			r = exponencial(n1,n2);
+    			printf("\n%.2f", r);
+    			break;
+    		
+   		 	case 'l':
+    			r = calcula_log(n1, n2);
+    			printf("log(%.2lf)%.2lf = %.2lf", n1, n2, r);
+    			break;
+    		case 'L':
+    			r = calcula_log(n1, n2);
+    			printf("log(%.2lf)%.2lf = %.2lf", n1, n2, r);
+    			break;
+    		
+	    	case 'v':
+    			r = raizquadrada(n2);
+    			r = r * n1;
+    			printf("\n%.2f", r);
+    			break;
+    			
+    		case 'V':
+    			r = raizquadrada(n2);
+    			r = r * n1;
+    			printf("\n%.2f", r);
+    			break;
+    		
+    		default:
+    			printf("Operação não reconhecida");
+    			exit(1);
+    	}	
 		}
 	
 			printf("\nSe deseja continuar na calculadora digite 0\nSe não qualquer número:\n");
@@ -51,6 +157,6 @@ main(){
     			cont++;
     		}
     	}
-	}
+	
 
 
